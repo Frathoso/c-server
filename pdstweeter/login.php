4f0cd42dee2d2d600102644cff3e6c9fac942f1e
Francis Sowani <frathoso@gmail.com>
2013-03-10 06:25:37 -0400

    Add server

--- pdstweeter/login.php
+++ pdstweeter/login.php
@@ -7,11 +7,53 @@ define("KEY_PASSWORD", 'password');
 define("KEY_FIRST_NAME", 'firstName');
 define("KEY_LAST_NAME", 'lastName');
 
+define("SERVER_PORT", 8888);
+define("SERVER_ADDR", 'localhost');
+define("BUFFER_SIZE", 2048);
+define("SUCCESS", "yes");
+
 $loginEmail = "";
 
 session_start();
 
-if (isset($_POST[KEY_EMAIL]) && isset($_POST[KEY_PASSWORD])) {
+// Submit email and password for authentication
+if(isset($_POST[KEY_EMAIL]) && isset($_POST[KEY_PASSWORD])){
+	try{
+		// Connect to the remote host
+		$hostIp = gethostbyname(SERVER_ADDR);
+		$socket = socket_create(AF_INET, SOCK_STREAM, SOL_TCP);
+		
+		if(socket_connect($socket, $hostIp, SERVER_PORT)){
+			// Submit email and password for authentication
+			$message = 'LOGIN '.$_POST[KEY_EMAIL].' '.$_POST[KEY_PASSWORD];
+			socket_write($socket, $message, strlen($message));
+			
+			// Analyse the returned results
+			$results = socket_read($socket, BUFFER_SIZE, PHP_NORMAL_READ);
+			echo "<br>Results (".$results.")";
+			if($results != ""){
+				if (strcmp(trim($results), SUCCESS) == 0) {
+					// Start user session and redirects to the user's homepage
+					$_SESSION[KEY_EMAIL] = $_POST[KEY_EMAIL];
+					header('Location: ' . HOME_PAGE);
+					exit();
+				}
+			}
+			// Remain in login page
+			echo "<br>Invalid username or password";
+			$loginEmail = $_POST[KEY_EMAIL];
+		}else{
+			echo "Error Connecting: ".socket_strerror(socket_last_error());
+		}
+		socket_close($socket);
+	}catch(exception $ex){
+		echo "<br>Exception:".$ex;
+	}
+}
+
+//************
+
+if (isset($_POST[KEY_EMAIL]) && isset($_POST[KEY_PASSWORD]) && 1 == 3) {
 	$fileUsers = file(DATA_ROOT . USERS_FILENAME);
 	if (isValidUser($fileUsers, $_POST[KEY_EMAIL], $_POST[KEY_PASSWORD])) {
 		// Start user session and redirects to the user's homepage
